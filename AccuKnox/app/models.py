df4c0c7211d4a37ef8f5e3592f04a0d1e88a1e93
Santosh Basragan <112722023+Sbsantosh8@users.noreply.github.com>
2024-09-12 21:20:30 +0530

    second commit

--- AccuKnox/app/models.py
+++ AccuKnox/app/models.py
@@ -1,11 +1,10 @@
 # employees/models.py
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-import time
-import logging
 
-logger = logging.getLogger(__name__)
+
+
+
+
 
 
 class Employee(models.Model):
@@ -25,12 +24,3 @@ class Employee(models.Model):
         return f"{self.first_name} {self.last_name} ({self.email})"
 
 
-@receiver(post_save, sender=Employee)
-def employee_post_save_handler(sender, instance, created, **kwargs):
-    logger.info("Signal handler started.")
-    time.sleep(5)  # Simulate a long-running task
-    if created:
-        logger.info(f"New employee created: {instance.email}")
-    else:
-        logger.info(f"Employee updated: {instance.email}")
-    logger.info("Signal handler finished.")
--- AccuKnox/app/signals.py
+++ AccuKnox/app/signals.py
@@ -1,14 +1,18 @@
 
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from .models import Employee
-# import logging
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Employee
+import time
+import logging
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
-# @receiver(post_save, sender=Employee)
-# def employee_post_save_handler(sender, instance, created, **kwargs):
-#     if created:
-#         logger.info(f"New employee created: {instance.first_name} {instance.last_name} ({instance.email})")
-#     else:
-#         logger.info(f"Employee updated: {instance.first_name} {instance.last_name} ({instance.email})")
+@receiver(post_save, sender=Employee)
+def employee_post_save_handler(sender, instance, created, **kwargs):
+    logger.info("Signal handler started.")
+    time.sleep(5)
+    if created:
+        logger.info(f"New employee created: {instance.first_name} {instance.last_name} ({instance.email})")
+    else:
+        logger.info(f"Employee updated: {instance.first_name} {instance.last_name} ({instance.email})")
+    logger.info("Signal handler finished.")
