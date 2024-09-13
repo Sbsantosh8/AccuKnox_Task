f23cedc111c04c845c088bb1fdffe6160214f53c
Santosh Basragan <112722023+Sbsantosh8@users.noreply.github.com>
2024-09-13 14:09:22 +0530

    Checking for repeated Code

--- AccuKnox/app/signals.py
+++ AccuKnox/app/signals.py
@@ -2,17 +2,32 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Employee
+from django.db import transaction
+import threading
 import time
 import logging
 
 logger = logging.getLogger(__name__)
 
+
+
 @receiver(post_save, sender=Employee)
 def employee_post_save_handler(sender, instance, created, **kwargs):
     logger.info("Signal handler started.")
-    time.sleep(5)
-    if created:
-        logger.info(f"New employee created: {instance.first_name} {instance.last_name} ({instance.email})")
-    else:
-        logger.info(f"Employee updated: {instance.first_name} {instance.last_name} ({instance.email})")
-    logger.info("Signal handler finished.")
+    time.sleep(4) # delaying 4 seconds
+    logger.info(f"Signal handler thread ID: {threading.get_ident()}")
+
+    # To check if it runs in the same transaction
+    try:
+        with transaction.atomic():
+            if created:
+                logger.info(f"New employee created: {instance.first_name} {instance.last_name} ({instance.email})")
+            else:
+                logger.info(f"Employee updated: {instance.first_name} {instance.last_name} ({instance.email})")
+            
+            logger.info("Raising exception to simulate a rollback.")
+            raise Exception("Simulating an error to rollback transaction")
+    except Exception as e:
+        logger.error(f"Exception in signal handler: {e}")
+    finally:
+        logger.info("Signal handler finished.")
--- AccuKnox/app/views.py
+++ AccuKnox/app/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, redirect
 from .forms import EmployeeRegistrationForm
 from .models import Employee
 from django.http import HttpResponse
-
-
+import threading
 import logging
+
 logger = logging.getLogger(__name__)
 
 def register_employee(request):
@@ -13,13 +13,16 @@ def register_employee(request):
         form = EmployeeRegistrationForm(request.POST)
         if form.is_valid():
             logger.info("Before Saving ...")
-            form.save()
+            print(f"Signal Caller thread ID: {threading.get_ident()}")
+
+            form.save()  # This triggers the post_save signal
             logger.info("After Saving....")
             return HttpResponse('Employee Registered  Successfully!! ')
     else:
         form = EmployeeRegistrationForm()
 
     return render(request, 'register.html', {'form': form})
+
 
 def employee_list(request):
     employees = Employee.objects.all()
